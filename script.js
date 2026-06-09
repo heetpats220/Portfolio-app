4e102041f65da5fd3baea35d10ca86bc05d507d5
heet patidar <heetpatidar007@gmail.com>
2026-06-09 17:58:27 +0530

    fast logo

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ window.addEventListener("load", () => {
     setTimeout(() => {
         document.querySelector(".splash").style.display = "none";
         document.querySelector(".main-content").style.display = "block";
-    }, 1500); // 2.5 seconds
+    }, 500); // 2.5 seconds
 });
