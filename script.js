c08f3cb0f1268ef17d6c4aaa4f29726cf8ec8929
heet patidar <heetpatidar007@gmail.com>
2026-06-09 18:32:28 +0530

    chk

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ window.addEventListener("load", () => {
     setTimeout(() => {
         document.querySelector(".splash").style.display = "none";
         document.querySelector(".main-content").style.display = "block";
-    }, 500); // 2.5 seconds
+    }, 500);
 });
