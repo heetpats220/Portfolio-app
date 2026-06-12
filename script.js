cc3509731a657187847f1303ac4f87717e55e930
heet patidar <heetpatidar007@gmail.com>
2026-06-12 14:52:53 +0530

    home page

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", () => {
     setTimeout(() => {
         document.querySelector(".splash").style.display = "none";
-        document.querySelector(".main-content").style.display = "block";
+        document.querySelector(".main-content").style.display = "flex";
     }, 500);
 });
