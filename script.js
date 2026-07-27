ee173171bbc85c153e906920d0588f8ababb66b6
heet patidar <heetpatidar007@gmail.com>
2026-07-27 22:35:12 +0530

    light dark mode home screen

--- script.js
+++ script.js
@@ -43,3 +43,46 @@ const observer = new IntersectionObserver(entries => {
 posts.forEach(post=>{
     observer.observe(post);
 });
+
+const mode = document.querySelector(".edit");
+const layout_main = document.querySelector(".layout_main");
+const header = document.querySelector(".header");
+const splash = document.querySelector(".splash");
+// let dark = true;
+function toggleTheme(){
+    if(!dark){
+    mode.innerHTML = `<img src="./static/svgviewer-output (14).svg" alt="">`;
+    console.log(dark);
+    layout_main.classList.add("light");
+    posts.forEach(post=>{
+        post.classList.add("post-light");
+    })
+    header.style.borderWidth = "0px";
+    header.style.color = "#962fbf";
+    // console.log(splash.innerHTML);
+    splash.innerHTML = `<img src="static/instagram (1).png" alt="">`
+
+    }
+    else{
+    mode.innerHTML = `<img src="./static/svgviewer-output (13).svg" alt="">`;
+    console.log(dark);
+    layout_main.classList.remove("light");
+    posts.forEach(post=>{
+        post.classList.remove("post-light");
+    })
+    header.style.borderWidth = "1px";
+    header.style.color = "White";
+    splash.innerHTML = `<img src="static/downloadlogoinsta.png" alt="">`
+
+    }
+}
+let dark = localStorage.getItem("theme") === "dark";
+toggleTheme();
+mode.addEventListener('click',()=>{
+    dark = !dark;
+    localStorage.setItem("theme", dark ? "dark" : "light");
+  
+    toggleTheme();
+
+
+});
